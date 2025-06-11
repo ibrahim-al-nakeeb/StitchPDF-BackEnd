283f51455a11a32bd25bd77ebe631a239f4c3ac0
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-12 02:54:00 +0300

    Refactor download lambda to use env config and build_response helper

--- generate_download_presigned_url.py
+++ generate_download_presigned_url.py
@@ -4,35 +4,40 @@ import os
 
 s3_client = boto3.client('s3')
 BUCKET = os.environ.get('BUCKET_NAME')
-EXPIRATION = 30  # URL expiration time in seconds
+EXPIRATION = os.environ.get('EXPIRATION')  # URL expiration time in seconds
+ALLOWED_ORIGIN = os.environ.get('ALLOWED_ORIGIN')
 
 def lambda_handler(event, context):
 	query = event.get('queryStringParameters') or {}
 	group_id = query.get('groupId')
 
 	if not group_id:
-		return {
-			'statusCode': 400,
-			'body': 'Missing "group_id" in request'
-		}
+		return build_response(400, {
+			'errorMessage': 'Missing "group_id" in request'
+		})
 
 	try:
 		url = s3_client.generate_presigned_url(
 			ClientMethod='get_object',
-			Params={'Bucket': BUCKET_NAME, 'Key': f'{group_id}/merged_output.pdf.pdf'},
+			Params={'Bucket': BUCKET, 'Key': f'{group_id}/merged_output.pdf'},
 			ExpiresIn=EXPIRATION
 		)
 		
-		return {
-			'statusCode': 200,
-			'body': json.dumps({'url': url})
-		}
+		return build_response(200, {
+			'presigned_url': url
+		})
 		
 	except Exception as e:
-		return {
-			'statusCode': 500,
-			'headers': {
-				'Access-Control-Allow-Origin': '*'
-			},
-			'body': str(e)
+		return build_response(500, {
+			'errorMessage': str(e)
+		})
+
+def build_response(status_code, body):
+	return {
+		'statusCode': status_code,
+		'body': json.dumps(body),
+		'headers': {
+			'Access-Control-Allow-Origin': ALLOWED_ORIGIN,
+			'Content-Type': 'application/json'
 		}
+	}

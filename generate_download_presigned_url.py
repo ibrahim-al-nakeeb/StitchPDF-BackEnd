c88a4f60da4f1b165300e1cddf0a0dab8b4a50bf
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-14 22:36:16 +0300

    Add S3 existence check before generating presigned download URL

--- generate_download_presigned_url.py
+++ generate_download_presigned_url.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+from botocore.exceptions import ClientError
 
 s3_client = boto3.client('s3')
 BUCKET = os.environ.get('BUCKET_NAME')
@@ -16,7 +17,11 @@ def lambda_handler(event, context):
 			'errorMessage': 'Missing "group_id" in request'
 		})
 
+	key = f'{group_id}/merged_output.pdf'
+
 	try:
+		s3_client.head_object(Bucket=BUCKET, Key=key)
+
 		url = s3_client.generate_presigned_url(
 			ClientMethod='get_object',
 			Params={'Bucket': BUCKET, 'Key': f'{group_id}/merged_output.pdf'},
@@ -27,6 +32,16 @@ def lambda_handler(event, context):
 			'presigned_url': url
 		})
 		
+	except ClientError as e:
+		if e.response['Error']['Code'] in ['404', 'NoSuchKey']:
+			return build_response(404, {
+				'errorMessage': f'File not found: {key}'
+			})
+		else:
+			return build_response(500, {
+				'errorMessage': f'ClientError: {str(e)}'
+			})
+
 	except Exception as e:
 		return build_response(500, {
 			'errorMessage': str(e)
